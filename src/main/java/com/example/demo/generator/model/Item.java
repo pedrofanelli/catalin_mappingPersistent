0b78f4971b13cad9e620e48a38c8f819e44be21f
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-11-16 19:15:32 -0300

    package and building entity

---
+++ src/main/java/com/example/demo/generator/model/Item.java
@@ -0,0 +1,44 @@
+package com.example.demo.generator.model;
+
+import java.util.Date;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Item {
+
+	@Id
+    @GeneratedValue(generator = "ID_GENERATOR")
+    private Long id;
+
+    @NotNull
+    @Size(
+            min = 2,
+            max = 255,
+            message = "Name is required, maximum 255 characters."
+    )
+    private String name;
+
+    @Future
+    protected Date auctionEnd;
+
+    public Long getId() { // Optional but useful
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getAuctionEnd() {
+        return auctionEnd;
+    }
+
+    public void setAuctionEnd(Date auctionEnd) {
+        this.auctionEnd = auctionEnd;
+    }
+}
---
+++ src/main/java/com/example/demo/generator/package-info.java
@@ -0,0 +1,11 @@
+
+/**
+ * 
+ * In a Spring Boot project, the package-info.java file is typically used to define package-level 
+ * annotations and documentation. This file can be placed in any package to provide additional information 
+ * about the package and its contents.
+ * 
+ * @author peter
+ *
+ */
+package com.example.demo.generator;
