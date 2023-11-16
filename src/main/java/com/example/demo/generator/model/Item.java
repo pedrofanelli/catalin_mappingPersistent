486b621ab0a3dd5aaf58dd7b5bb5a4c4d0399818
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-11-16 19:22:11 -0300

    imported hibernate validator

--- src/main/java/com/example/demo/generator/model/Item.java
+++ src/main/java/com/example/demo/generator/model/Item.java
@@ -3,6 +3,9 @@ package com.example.demo.generator.model;
 import java.util.Date;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 @Entity
 public class Item {
