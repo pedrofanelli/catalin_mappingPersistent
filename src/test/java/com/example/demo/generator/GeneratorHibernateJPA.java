bade1c4c8ecf6633ca2612ec7524dd44250385a6
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-11-16 20:35:40 -0300

    building example for hibernate jpa

---
+++ src/test/java/com/example/demo/generator/GeneratorHibernateJPA.java
@@ -0,0 +1,53 @@
+package com.example.demo.generator;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+import com.example.demo.generator.model.Item;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+public class GeneratorHibernateJPA {
+
+	@Test
+    public void storeLoadItem() {
+
+        EntityManagerFactory emf =
+                Persistence.createEntityManagerFactory("ch05.generator");
+        EntityManager em = emf.createEntityManager();
+
+        try {
+            em.getTransaction().begin();
+
+            Item item = new Item();
+            item.setName("Some Item");
+            item.setAuctionEnd(Helper.tomorrow());
+
+            em.persist(item);
+
+            em.getTransaction().commit();
+            em.getTransaction().begin();
+
+            List<Item> items =
+                    em.createQuery("select i from Item i", Item.class).getResultList();
+            //SELECT * from ITEM
+
+            em.getTransaction().commit();
+
+            assertAll(
+                    () -> assertEquals(1, items.size()),
+                    () -> assertEquals("Some Item", items.get(0).getName())
+            );
+
+        } finally {
+            em.close();
+            emf.close();
+        }
+    }
+}
